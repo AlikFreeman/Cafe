827fd91faecf1fb93ef8ebc0a42c13bc6428c02b
AlikFreeman <Ibizza74@gmail.com>
2022-04-12 15:15:07 +0300

    Liquibase start

---
+++ src/test/java/ru/test/cafe/cafedemo/CafeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.cafe.cafedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
