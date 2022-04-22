00860ad4efaebacd0ac5ebdb92c51ee752aff093
AlikFreeman <Ibizza74@gmail.com>
2022-04-22 10:06:03 +0300

    Тест

--- src/test/java/ru/test/cafe/cafedemo/service/coffeGrade/OrderCalcServiceImplTest.java
+++ src/test/java/ru/test/cafe/cafedemo/service/coffeGrade/OrderCalcServiceImplTest.java
@@ -1,12 +1,34 @@
 package ru.test.cafe.cafedemo.service.coffeGrade;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.test.cafe.cafedemo.dto.OrderPointDto;
+import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class OrderCalcServiceImplTest {
-@Autowired
+    @Autowired
     private OrderCalcServiceImpl service;
+    @ParameterizedTest
+    @MethodSource("createTestData")
+    void firstTest (List<OrderPointDto> orderPointDtos, Integer resultSum) {
+        Integer results = service.calc(orderPointDtos);
+        Assertions.assertEquals(results, resultSum);
+    }
+    private static Stream<Arguments> createTestData() {
+        return Stream.of(Arguments.of(List.of(createOrderPointDto()), 6));
+    }
+
+    private static OrderPointDto createOrderPointDto() {
+        OrderPointDto dto = new OrderPointDto();
+        dto.setCoffeeGradeId(1);
+        dto.setCupCounter(5);
+        return dto;
+    }
 
 }
